4cd98befd42c9e5890c73e2acc4e7934bad6b94a
Muzaki29 <emailmu@gmail.com>
2026-02-02 10:23:58 +0700

    Fix: Ensure inactive products are hidden in catalog and detail views

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -9,12 +9,16 @@ class CatalogController extends Controller
 {
     public function index()
     {
-        $products = Product::with('variants')->get();
+        $products = Product::where('is_active', true)->with('variants')->get();
         return view('catalog.index', compact('products'));
     }
 
     public function show(Product $product)
     {
+        if (!$product->is_active) {
+            abort(404);
+        }
+        
         $product->load('variants');
         return view('catalog.show', compact('product'));
     }
